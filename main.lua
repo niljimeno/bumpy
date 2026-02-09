571d71fa6bf3701798e90c1f9940a2998c3403af
niljimeno <niliara@proton.me>
2026-02-09 13:48:51 +0100

    update

--- main.lua
+++ main.lua
@@ -8,6 +8,10 @@ function love.load()
     state.player = player.new()
 end
 
+function love.update(dt)
+    player.update(dt, state.player)
+end
+
 function love.draw()
     player.draw(state.player)
 end
--- player.lua
+++ player.lua
@@ -3,12 +3,36 @@ local math = require("utils.math")
 function newPlayer()
     local instance = {
 	position = math.vector(20, 40),
-
-	speed = math.vector(),
+	velocity = math.vector(),
+	speed = 3000,
     }
 
     return instance
 end
+
+function accelerate(dt, player)
+    local change = player.speed * dt
+    player.velocity.x = player.velocity.x + change
+end
+
+function decelerate(dt, player)
+    local change = player.speed * dt
+    player.velocity.x = player.velocity.x - change
+end
+
+function move(dt, player)
+    player.position.x = player.position.x + (player.velocity.x*dt)
+end
+
+function updatePlayer(dt, player)
+    if (love.keyboard.isDown("space")) then
+	accelerate(dt, player)
+    else
+	decelerate(dt, player)
+    end
+    move(dt, player)
+end
+
 
 function drawPlayer(player)
     love.graphics.circle("fill", player.position.x, player.position.y, 20)
@@ -16,5 +40,6 @@ end
 
 return {
     new = newPlayer,
+    update = updatePlayer,
     draw = drawPlayer,
 }
