4c0e0ce23f3865c72046ac0fdcb202e7992d5697
alvardroid <alvardroid@gmail.com>
2026-02-11 13:29:15 +0100

    background

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 world = require("world")
 player = require("player")
+background = require("background")
 
 state = {
     player = {}
@@ -8,13 +9,16 @@ state = {
 function love.load()
     world.load()
     state.player = player.new()
+    background.load()
 end
 
 function love.update(dt)
+    background.update(dt)
     player.update(dt, state.player)
 end
 
 function love.draw()
+    background.draw()
     world.draw()
     player.draw(state.player)
 end
