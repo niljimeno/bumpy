25e3bcf73d602044537accdfe2289b117849b2cf
niljimeno <niliara@proton.me>
2026-02-11 12:01:37 +0100

    update moves

--- player.lua
+++ player.lua
@@ -3,25 +3,31 @@ local math = require("utils.math")
 function newPlayer()
     local instance = {
 	position = math.vector(20, 40),
-	velocity = math.vector(),
-	speed = 3000,
+	velocity = 0,
+	maxVelocity = 20,
+	direction = 0,
+	speed = 30,
     }
 
     return instance
 end
 
 function accelerate(dt, player)
-    local change = player.speed * dt
-    player.velocity.x = player.velocity.x + change
+    local change = player.speed
+    player.velocity = player.velocity + change * dt
 end
 
 function decelerate(dt, player)
-    local change = player.speed * dt
-    player.velocity.x = player.velocity.x - change
+    local change = player.speed * 2
+    player.velocity = player.velocity - change * dt
 end
 
 function move(dt, player)
-    player.position.x = player.position.x + (player.velocity.x*dt)
+    player.position:add(
+	degreeToVector(
+	    player.direction,
+	    player.velocity*player.speed*dt))
+    print(player.position.x, player.position.y)
 end
 
 function updatePlayer(dt, player)
@@ -30,6 +36,7 @@ function updatePlayer(dt, player)
     else
 	decelerate(dt, player)
     end
+    player.velocity = clamp(player.velocity, 0, player.maxVelocity)
     move(dt, player)
 end
 
--- utils/math.lua
+++ utils/math.lua
@@ -1,11 +1,26 @@
+local math = require "math"
+
 function vector(x, y)
     x = x or 0
     y = y or 0
+    add = function(self, vec)
+	self.x = self.x + vec.x
+	self.y = self.y + vec.y
+    end
     
     return {
 	x=x,
 	y=y,
+	add=add
     }
+end
+
+function degreeToVector(deg, scale)
+    scale = scale or 1
+    return vector(
+	math.sin(deg) * scale,
+	math.cos(deg) * scale
+    )
 end
 
 function lerp(a, b, t)
