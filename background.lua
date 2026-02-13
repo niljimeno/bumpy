424a18fa4b205fa84192f0008ad24ef7f899fed1
niljimeno <niliara@proton.me>
2026-02-13 12:20:06 +0100

    optimize background

--- background.lua
+++ background.lua
@@ -1,33 +1,45 @@
-
 local Background = {}
 local game_width, game_height
 local image_width, image_height
 local background_image 
-local grid_pos = 0
+local gridPos = 0
+local squareSize = 60
 
 function Background.load()
     background_image = love.graphics.newImage("assets/background.png")
-    game_width, game_height = love.graphics.getDimensions()
-    image_height = background_image:getHeight()
-    image_width = background_image:getWidth()
 end
 
 function Background.update(dt)
-    grid_pos = grid_pos + dt * 0.15
-    if grid_pos >= image_width then
-        grid_pos = 0
+    gridPos = gridPos + dt * 10
+    if gridPos >= squareSize then
+        gridPos = gridPos - squareSize
     end
 end
 
 function Background.draw()
-    for i = -128, game_height do
-        for j = -128, game_width do
-            love.graphics.draw(
-                background_image,
-                (j + grid_pos) * image_width, (i + grid_pos) * image_height
-            )
+    love.graphics.clear( 0.1, 0.1, 0.1, 1, true, true )
+    love.graphics.setColor( 0.4, 0.4, 0.4, 1 )
+
+    local gameWidth, gameHeight = love.graphics.getDimensions()
+    local h = gameHeight / (squareSize * 2)
+    local w = gameWidth / (squareSize * 2)
+    
+    for i = -2, h do
+        for j = -2, w do
+	    love.graphics.rectangle("fill",
+				    j*squareSize*2+gridPos,
+				    i*squareSize*2+gridPos,
+				    squareSize,
+				    squareSize)
+	    love.graphics.rectangle("fill",
+				    j*squareSize*2 + squareSize + gridPos,
+				    i*squareSize*2 + squareSize + gridPos,
+				    squareSize,
+				    squareSize)
         end
     end
+
+    love.graphics.setColor( 1, 1, 1, 1 )
 end
 
 return Background
